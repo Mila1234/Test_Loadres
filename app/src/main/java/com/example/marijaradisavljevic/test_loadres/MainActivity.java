178f350533fc36166cc093bc3ed688f3c87dd425
marija.radisavljevic <marija.radisavljevic@asseco-see.rs>
2016-05-09 11:36:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marijaradisavljevic/test_loadres/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.marijaradisavljevic.test_loadres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
